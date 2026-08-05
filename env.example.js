4913b110637cda5cf8e2c2b269bff19fe945ad30
khususfixmrah-max <khususfixmrah@gmail.com>
2026-08-06 06:59:37 +0700

    Add files via upload

---
+++ env.example.js
@@ -0,0 +1,10 @@
+# GoBiz / GoPay Credentials
+GOPAY_EMAIL=discordsaren@gmail.com
+GOPAY_PASSWORD=Kyrienatons1
+
+# String QRIS statis milik merchant (didapat dari gambar QRIS GoPay kamu)
+# Bisa scan gambar QRIS menggunakan scanner, lalu paste hasilnya di sini
+QRIS_STRING=00020101021126610014COM.GO-JEK.WWW01189360091437369064250210G7369064250303UMI51440014ID.CO.QRIS.WWW0215ID10243566705790303UMI5204581653033605802ID5921Violet Silence, Gubug6008GROBOGAN61055816462070703A0163045E6A...
+
+# Nominal pembayaran default (dalam Rupiah), dipakai jika tidak ada argumen CLI
+PRICE_AMOUNT=2000
